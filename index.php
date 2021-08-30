8fefba4c24f6713dd3e57d04c5977c037e28810e
Sachin Sharma <sachinmodgil6@gmail.com>
2021-08-30 18:40:51 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$your_url = "https://www.youtube.com/watch?v=PGwH2e8MikM";
+function get_youtube_id_from_url($url) {
+    if (stristr($url, 'youtu.be/')) {
+        preg_match('/(https:|http:|)(\/\/www\.|\/\/|)(.*?)\/(.{11})/i', $url, $final_ID);
+        return $final_ID[4];
+    } else {
+        @preg_match('/(https:|http:|):(\/\/www\.|\/\/|)(.*?)\/(embed\/|watch.*?v=|)([a-z_A-Z0-9\-]{11})/i', $url, $IDD);
+        return $IDD[5];
+    }
+}
+$video_id = get_youtube_id_from_url($your_url);
+?>
